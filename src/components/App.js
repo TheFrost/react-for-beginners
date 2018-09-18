90b3feb93ef454d08033305684dc3b35e28d5ad4
ftinoco <ftinoco@ia.com.mx>
2018-09-18 17:34:57 -0500

    Removing Items from State

--- src/components/App.js
+++ src/components/App.js
@@ -48,6 +48,14 @@ class App extends React.Component {
     // 3. Set that to state
     this.setState({ fishes });
   }
+  deleteFish = key => {
+    // 1. Take a copy of state
+    const fishes = { ...this.state.fishes };
+    // 2. Update the state
+    fishes[key] = null;
+    // 3. Update the state
+    this.setState({ fishes });
+  }
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -57,6 +65,14 @@ class App extends React.Component {
     // 2. Either add to the order, or update the number in our order
     order[key] = order[key] + 1 || 1;
     // 3. Call setState to update our state object
+    this.setState({ order });
+  }
+  removeFromOrder = key => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. remove that item from order
+    delete order[key];
+    // 3. Update the state
     this.setState({ order });
   }
   render() {
@@ -75,10 +91,15 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order 
+          fishes={this.state.fishes} 
+          order={this.state.order} 
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
--- src/components/Order.js
+++ src/components/Order.js
@@ -7,14 +7,14 @@ class Order extends React.Component {
     const count = this.props.order[key];
     const isAvailable = fish && fish.status === 'available';
     // Make sure the fish is loaded before we continue
-    if (!fish) return null;
+    if (!fish || !count) return null;
     if (!isAvailable) {
       return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>;
     }
     return (
       <li key={key}>
-        {count} lbs {fish.name}
-        {formatPrice(count * fish.price)}
+        {count} lbs {fish.name} {formatPrice(count * fish.price)}
+        <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
       </li>
     );
   }
